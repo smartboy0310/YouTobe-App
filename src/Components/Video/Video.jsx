6a54594cf8a1ef9f1a2e8ed9f9ea4cf5c7a1f574
smartboy0310 <qdustudent6@gmail.com>
2021-11-29 17:18:31 +0500

    Add

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Video.css';
 
 import Header from '../Header/Header';
-import Menu from '../Menu/Menu';
 import SingelVideo from './SingelVideo/SingelVideo';
 function Video () {
    return (
